a595274916fc51aa6b0b3fd090aae786774576b9
Oleksiy <oleksiypolshchak@gmail.com>
2016-06-08 21:31:42 +0300

    front-end

--- ui/src/test/java/com/dealgrain/UiApplicationTests.java
+++ ui/src/test/java/com/dealgrain/UiApplicationTests.java
@@ -9,8 +9,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @SpringApplicationConfiguration(classes = UiApplication.class)
 public class UiApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+
+    }
 
 }
